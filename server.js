79fcfd7c207c4cc91bcbaf488321df850d1190a7
Ben Winding <ben.winding@gmail.com>
2017-10-07 01:07:21 +1030

    Using express and pug now :)

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const path = require("path");
+const app = new express();
+
+app.set('view engine', 'pug');
+app.use(express.static(path.join(__dirname, 'public')));
+
+const siteConfig = require('./content/portalGridConfig.json');
+app.get('/', function (req, res) {
+  res.render('index', siteConfig)
+});
+
+const PORT = process.env.PORT || 8000;
+app.listen(PORT);
+console.log('listening on port: ' + PORT);
