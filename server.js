549f1600c3cc284cdab7b199a8245791077d0c79
Ben Winding <ben.winding@gmail.com>
2017-10-21 01:05:19 +1030

    Google verification

--- server.js
+++ server.js
@@ -6,6 +6,10 @@ app.set('view engine', 'pug');
 app.set('views', path.join(__dirname, '/public'));
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.get('/google91ab19b9428bca2d.html', function (req, res) {
+  res.renderFile('google91ab19b9428bca2d.html');
+});
+
 const siteConfig = require('./public/portal/portal.json');
 app.get('/', function (req, res) {
   res.render('portal/portal', siteConfig)
