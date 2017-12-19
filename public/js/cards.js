40e82f23e368ee93c249e716404f2ac099980c79
Ben Winding <ben.winding@gmail.com>
2017-12-19 23:58:16 +1030

    Working with the Muuri dragable grid library

---
+++ public/js/cards.js
@@ -0,0 +1,24 @@
+// To align portal cards in grid
+var grid = new Muuri('.grid', {
+    dragEnabled: true,
+    layoutOnInit: false,
+    sortData: {
+        id: function (item, element) {
+            return parseFloat(element.children[0].textContent);
+        }
+    }
+});
+
+// Adjusting container width to resize to align with cards exactly
+var  gridEl = document.querySelector('.grid');
+var  itemEl = document.querySelector('.item');
+var  origRefreshDimensions = Muuri.prototype._refreshDimensions;
+Muuri.prototype._refreshDimensions = function () {
+    const itemWidth = itemEl.clientWidth;
+    const outerMarginPx = 5;
+    const columnsCount = 4;
+    const totalWidthPx = (itemWidth * columnsCount) + (outerMarginPx * 2);
+    gridEl.style.width = (totalWidthPx * 100 / document.body.clientWidth) + "%";
+
+    return origRefreshDimensions.call(this);
+};
