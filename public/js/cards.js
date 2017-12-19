71a51eb1facab996986d200c64f73e493e34a1bd
Ben Winding <ben.winding@gmail.com>
2017-12-20 00:15:15 +1030

    Working with snappy resizing!

--- public/js/cards.js
+++ public/js/cards.js
@@ -1,7 +1,6 @@
 // To align portal cards in grid
 var grid = new Muuri('.grid', {
     dragEnabled: true,
-    layoutOnInit: false,
     layoutOnInit: true,
     sortData: {
         id: function (item, element) {
@@ -15,11 +14,14 @@ var  gridEl = document.querySelector('.grid');
 var  itemEl = document.querySelector('.item');
 var  origRefreshDimensions = Muuri.prototype._refreshDimensions;
 Muuri.prototype._refreshDimensions = function () {
-    const itemWidth = itemEl.clientWidth;
-    const outerMarginPx = 5;
-    const columnsCount = 4;
-    const totalWidthPx = (itemWidth * columnsCount) + (outerMarginPx * 2);
-    gridEl.style.width = (totalWidthPx * 100 / document.body.clientWidth) + "%";
+    const qdocWidthPx = document.body.clientWidth;
+    if(qdocWidthPx > 700) {
+        const qitemWidthPx = itemEl.clientWidth;
+        const outerMarginPx = 5;
+        const columnsCount = Math.floor(qdocWidthPx / qitemWidthPx);
+        const totalWidthPx = (qitemWidthPx * columnsCount) + (outerMarginPx * 2);
+        gridEl.style.width = (totalWidthPx * 100 / qdocWidthPx) + "%";
+    }
 
     return origRefreshDimensions.call(this);
 };
