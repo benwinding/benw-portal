41080fe7175b59ff3c3b2d237a6ac3a058fe1903
Ben Winding <ben.winding@gmail.com>
2017-12-27 20:58:49 +1030

    Masonry js implemented with bootstrap

--- views/routes.js
+++ views/routes.js
@@ -1,17 +1,16 @@
 const express = require('express');
 const router = express.Router();
 
-const siteConfig = require('./portal/portal.json');
 router.get('/', function(req, res, next) {
-    res.render('portal', siteConfig)
+    res.render('portal');
 });
 
 router.get('/but-how', function(req, res, next) {
-    res.render('but-how')
+    res.render('but-how');
 });
 
 router.get('/about', function(req, res, next) {
-    res.render('about')
+    res.render('about');
 });
 
 module.exports = router;
