cdd4bb44e68ad804c4a78b503427fafb6a5b9e3e
U-BSC\ben <ben@bsc-newoffice.bsc.local>
2018-02-27 11:34:42 +1030

    SEO updates

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const path = require("path");
+var sitemap = require('express-sitemap');
 const app = new express();
 
 app.use(express.static(path.join(__dirname, 'public')));
@@ -10,6 +11,20 @@ app.set('views', path.join(__dirname, 'views'));
 
 const index = require('./views/routes');
 app.use('/', index);
+
+const sm = sitemap({
+	robots: 'public/robots.txt',
+  sitemap: 'public/sitemap.xml',
+  route: {
+    'ALL': {
+      lastmod: new Date().toLocaleDateString(),
+      changefreq: 'always',
+      priority: 1.0,
+    }
+  },
+});
+sm.generate(app);
+sm.toFile(); // write this map to file
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT);
