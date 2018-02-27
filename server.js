c84867d79c389842e531606bca4207fd2c3a353d
Ben Winding <ben.winding@gmail.com>
2018-02-27 13:05:27 +1030

    SEO updates

--- server.js
+++ server.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const path = require("path");
-var sitemap = require('express-sitemap');
 const app = new express();
 
 app.use(express.static(path.join(__dirname, 'public')));
@@ -12,20 +11,7 @@ app.set('views', path.join(__dirname, 'views'));
 const index = require('./views/routes');
 app.use('/', index);
 
-const sm = sitemap({
-	robots: 'public/robots.txt',
-  sitemap: 'public/sitemap.xml',
-  sitemapSubmission: '/sitemap.xml',
-  route: {
-    'ALL': {
-      lastmod: new Date().toLocaleDateString(),
-      changefreq: 'always',
-      priority: 1.0,
-    }
-  },
-});
-sm.generate(app);
-sm.toFile(); // write this map to file
+require('./sitemap')(app);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT);
