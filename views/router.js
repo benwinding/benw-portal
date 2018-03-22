9de2f5c071f8ad37fd62768530621b1078cbf33e
Ben Winding <ben.winding@gmail.com>
2018-03-22 13:19:00 +1030

    Adding old versions of the site

--- views/router.js
+++ views/router.js
@@ -1,11 +1,10 @@
 const express = require('express');
 const router = express.Router();
 YAML = require('yamljs');
-// Load yaml file using YAML.load
-const portalTiles = YAML.load('./views/portal/tiles.yaml');
 
+const portalTiles = YAML.load('./views/portal/tiles.yaml');
 router.get('/', function(req, res, next) {
-    res.render('portal', { layout : 'layout', json: portalTiles });
+    res.render('portal', { layout : 'layout', inputTiles: portalTiles });
 });
 
 router.get('/but-how', function(req, res, next) {
@@ -18,6 +17,11 @@ router.get('/about', function(req, res, next) {
 
 router.get('/engineering', function(req, res, next) {
     res.render('engineering');
+});
+
+const oldSiteImages = YAML.load('./views/old-site/image-list.yaml');
+router.get('/old-site', function(req, res, next) {
+    res.render('old-site', {layout: 'layout', inputList: oldSiteImages});
 });
 
 module.exports = router;
