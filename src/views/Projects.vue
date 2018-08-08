d7668ebd01073cc1aa2e929d05dd5a2a66c5ee56
Ben Winding <ben.winding@gmail.com>
2018-08-09 02:16:55 +0930

    Filterable projects page

--- src/views/Projects.vue
+++ src/views/Projects.vue
@@ -1,8 +1,24 @@
 <template>
   <div>
     <rainbow-text text="Projects"></rainbow-text>
+    <div class="card-filters">
+      <ul class="chk-all">
+        <li>
+          <input type="checkbox" v-model="chkAll">
+          <span>All</span>
+        </li>
+      </ul>
+      <ul v-for="(tag) in tagsAll" :key="tag">
+        <li>
+          <input type="checkbox" :id="tag" :value="tag" v-model="tags">
+          <span>{{tag}}</span>
+        </li>
+      </ul>
+    </div>
     <div class="cards">
-      <card :project="project" v-for="(project, index) in projects" :key="index"></card>
+      <transition name="fade" mode="out-in" v-for="(project, index) in projects" :key="index">
+        <card :project="project"></card>
+      </transition>
     </div>
   </div>
 </template>
@@ -11,12 +27,42 @@
 import RainbowText from "@/components/RainbowText"
 import Card from "@/components/Card"
 
-import projects from "../data/projects.json"
+import projectsAll from "../data/projects.json"
+const tags = projectsAll.map((val) => val.tags)
+  .reduce((acc, cur) => acc.concat(cur))
+const tagsUnique = Array.from(new Set(tags))
 
 export default {
   data () {
     return {
-      projects
+      tags: tagsUnique,
+      chkAll: true
+    }
+  },
+  computed: {
+    tagsAll: function() {
+      return tagsUnique
+    },
+    projects: function() {
+      const activeTags = this.tags
+      const activeProjects = projectsAll.reduce((acc, cur) => {
+        for (const tag of cur.tags) {
+          if (activeTags.includes(tag)) {
+            acc.push(cur);
+            break;
+          }
+        }
+        return acc;
+      }, [])
+      return activeProjects;
+    }
+  },
+  watch: {
+    chkAll: function(val) {
+      if (val)
+        this.tags = this.tagsAll;
+      else
+        this.tags = [];
     }
   },
   components: {
@@ -27,6 +73,31 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+  .card-filters {
+    margin: 8px;
+  }
+
+  .card-filters > .chk-all {
+    background-color: lightgrey;
+  }
+  
+  .card-filters ul {
+    display: inline-block;
+    list-style: none;
+    margin: 8px 8px 0px 0px;
+    padding: 0px;
+    vertical-align: middle;
+    background-color: cyan;
+  }
+  .card-filters input {
+    margin: 10px;
+    vertical-align: middle;
+    transform: scale(2);
+  }
+  .card-filters span {
+    margin-right: 5px;
+    vertical-align: middle;
+  }
 
   .cards {
     display: grid;
@@ -80,6 +151,14 @@ export default {
   
   .cards div:nth-child(10n+10) {
     background-color: #ff8f00;
+  }
+
+  .fade-enter-active, .fade-leave-active {
+    transition: opacity 1s, transform 1s;
+  }
+  .fade-enter, .fade-leave-to {
+    opacity: 0;
+    transform: translateY(20%);
   }
 </style>
 
