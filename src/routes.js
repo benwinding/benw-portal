d6707c4a2108c1e25594299af52124277d6ee94a
Ben Winding <ben.winding@gmail.com>
2018-08-09 12:32:51 +0930

    Better routing segregation

---
+++ src/routes.js
@@ -0,0 +1,41 @@
+import Home from './views/Home.vue'
+import About from './views/About.vue'
+import Projects from './views/Projects.vue'
+import Contact from './views/Contact.vue'
+import How from './views/How.vue'
+import OldSite from './views/OldSite.vue'
+
+const routes = [
+  {
+    path: '/',
+    name: 'home',
+    component: Home
+  },
+  {
+    path: '/about',
+    name: 'about',
+    component: About
+  },
+  {
+    path: '/projects',
+    name: 'projects',
+    component: Projects
+  },
+  {
+    path: '/but-how',
+    name: 'but-how',
+    component: How
+  },
+  {
+    path: '/old-site',
+    name: 'old-site',
+    component: OldSite
+  },
+  {
+    path: '/contact',
+    name: 'contact',
+    component: Contact
+  }
+]
+
+export { routes }
