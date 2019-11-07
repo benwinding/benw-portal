e8c463bed99bd09c180159a9306cbce420d669a4
Ben Winding <ben.winding@gmail.com>
2019-11-07 19:49:26 +1030

    Added github feed to about page!

--- pages/about.vue
+++ pages/about.vue
@@ -1,19 +1,34 @@
 <template>
   <div>
+    <link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/octicons/2.0.2/octicons.min.css" />
+    <link
+      rel="stylesheet"
+      href="https://cdn.jsdelivr.net/gh/caseyscarborough/github-activity@master/src/github-activity.css"
+    />
+    <script
+      type="text/javascript"
+      src="//cdnjs.cloudflare.com/ajax/libs/mustache.js/0.7.2/mustache.min.js"
+    ></script>
+    <script
+      type="text/javascript"
+      src="https://cdn.jsdelivr.net/gh/caseyscarborough/github-activity@master/src/github-activity.js"
+    ></script>
     <rainbow-text text="About"></rainbow-text>
     <div class="bio-pic">
-      <img class="circular" src="/images/pic.jpg" alt="selfie!">
+      <img class="circular" src="/images/pic.jpg" alt="selfie!" />
       <div class="about-box">
         <h2>History</h2>
         <p>Growing up in Adelaide, South Australia. Ben began his life as a normal child, with all the prospects of becoming a contributing member of society. He's trying his best and will continue to do so.</p>
         <h2>Now</h2>
         <p>He intends to be a successful web developer, making the internet a better place for all those who use it.</p>
+        <h2>Development</h2>
+        <div id="feed"></div>
         <h2>Writing</h2>
         <transition-group name="list" tag="p">
           <div class="blog-item" v-for="(blog) in blogPosts" :key="blog.title">
-            <a  :href="blog.link">
-              <span class="date" > {{ blog.date }} </span>
-              <span class="title" > {{ blog.title }} </span>
+            <a :href="blog.link">
+              <span class="date">{{ blog.date }}</span>
+              <span class="title">{{ blog.title }}</span>
             </a>
           </div>
         </transition-group>
@@ -23,72 +38,81 @@
 </template>
 
 <script>
-import RainbowText from "~/components/RainbowText"
-import axios from "axios"
+import RainbowText from "~/components/RainbowText";
+import axios from "axios";
 
 function getBlogPosts() {
-  return axios.get('https://blog.benwinding.com/feed.json', {
-    json: true,
-    crossdomain: true,
-  })
-  .then(function (response) {
-    console.log(response);
-    return response.data;
-  })
+  return axios
+    .get("https://blog.benwinding.com/feed.json", {
+      json: true,
+      crossdomain: true
+    })
+    .then(function(response) {
+      console.log(response);
+      return response.data;
+    });
 }
 
 export default {
   head: {
     meta: [
-      { property:"og:image", content:"https://i.imgur.com/orqq5jB.jpg" },
-      { property:"og:title", content:"About - Ben Winding" },
-      { property:"og:description", content:"A web developer from Adelaide, South Australia." },
-    ],    
-    title: 'About'
+      { property: "og:image", content: "https://i.imgur.com/orqq5jB.jpg" },
+      { property: "og:title", content: "About - Ben Winding" },
+      {
+        property: "og:description",
+        content: "A web developer from Adelaide, South Australia."
+      }
+    ],
+    title: "About"
   },
   components: {
-    'rainbow-text': RainbowText,
+    "rainbow-text": RainbowText
   },
   data() {
     return {
       blogPosts: []
-    }
+    };
   },
-  mounted () {
+  mounted() {
     this.$nextTick(() => {
       getBlogPosts()
-      .then((data) => {
-        let posts = data.slice(0,6).map((item) => { 
-          return {
-            title: item.title,
-            date: new Date(item.date).toDateString(),
-            link: 'https://blog.benwinding.com/' + item.path
+        .then(data => {
+          let posts = data.slice(0, 6).map(item => {
+            return {
+              title: item.title,
+              date: new Date(item.date).toDateString(),
+              link: "https://blog.benwinding.com/" + item.path
+            };
+          });
+          posts.push({
+            link: "https://blog.benwinding.com/",
+            title: ". . . . . . . ."
+          });
+          let delayMs = 0;
+          for (let post of posts) {
+            delayMs += 300;
+            setTimeout(() => {
+              this.blogPosts.push(post);
+            }, delayMs);
           }
-        });
-        posts.push({
-          link: 'https://blog.benwinding.com/',
-          title: '. . . . . . . .',
         })
-        let delayMs = 0;
-        for (let post of posts) {
-          delayMs += 300;
-          setTimeout(() => {
-            this.blogPosts.push(post)
-          }, delayMs);
-        }
-      })
-      .catch(function (error) {
-        console.log(error);
-      });
-    })
-  },
-}
+        .catch(function(error) {
+          console.log(error);
+        });
+    });
+    GitHubActivity.feed({
+      username: "benwinding",
+      selector: "#feed",
+      limit: 20 // optional
+    });
+  }
+};
 </script>
 
 <style lang="scss" scoped>
 p {
   font-size: 18px;
-  background-color: #FFFFFFBB;
+  background-color: #ffffffbb;
 }
 
 .bio-pic {
@@ -118,9 +142,9 @@ p {
 }
 
 .circular:hover {
-  -ms-transform: rotate3d(0,1,0,360deg); 
-  -webkit-transform: rotate3d(0,1,0,360deg);
-  transform: rotate3d(0,1,0,360deg);
+  -ms-transform: rotate3d(0, 1, 0, 360deg);
+  -webkit-transform: rotate3d(0, 1, 0, 360deg);
+  transform: rotate3d(0, 1, 0, 360deg);
   -webkit-transition-duration: 3s;
   -moz-transition-duration: 3s;
   transition-duration: 3s;
@@ -142,12 +166,18 @@ p {
   }
 }
 
-.list-enter-active, .list-leave-active {
+.list-enter-active,
+.list-leave-active {
   transition: all 1s;
 }
 .list-enter, .list-leave-to /* .list-leave-active below version 2.1.8 */ {
   opacity: 0;
   transform: translateY(30px);
+}
+
+#feed {
+  height: 300px;
+  margin-bottom: 15px;
 }
 </style>
 
