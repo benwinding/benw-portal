a594d93791a34b1cabcc0dea7394f9f16d2380c1
Ben Winding <hello@benwinding.com>
2021-01-03 21:25:34 +1030

    Free donut page is working

---
+++ layouts/full-screen.vue
@@ -0,0 +1,22 @@
+<template>
+  <nuxt />
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="css">
+@import "https://fonts.googleapis.com/css?family=Lato:400,800";
+</style>
+
+<style lang="scss">
+@import "~/node_modules/bootstrap/scss/bootstrap.scss";
+
+html,
+body,
+#app {
+  height: 98%;
+  font-family: "Lato", sans-serif;
+}
+</style>
