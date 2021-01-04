8683ad4a03284d1addf7dc51f7c5c59dc878bb4d
Ben Winding <hello@benwinding.com>
2021-01-04 20:09:59 +1030

    Added link to donut

--- components/Header.vue
+++ components/Header.vue
@@ -1,40 +1,53 @@
 <template>
-  <div class="header">
-    <nuxt-link to="/">Home</nuxt-link>
-    <nuxt-link to="/about">About</nuxt-link>
-    <nuxt-link to="/projects">Projects</nuxt-link>
-    <nuxt-link to="/contact">Contact</nuxt-link>
+  <div>
+    <div class="header">
+      <nuxt-link to="/">Home</nuxt-link>
+      <nuxt-link to="/about">About</nuxt-link>
+      <nuxt-link to="/projects">Projects</nuxt-link>
+      <nuxt-link to="/contact">Contact</nuxt-link>
+    </div>
+    <a class="donut" href="/eat-the-donut.html"></a>
   </div>
 </template>
 
-<script>
-
-</script>
+<script></script>
 
 <style lang="scss" scoped>
-  .header {
-    margin-top: 20px;
-    text-align: right;
-    margin-right: 130px;
+.header {
+  margin-top: 20px;
+  text-align: right;
+  margin-right: 130px;
 
-    @media (max-width: 650px) {
-      text-align: left;
-    }
+  @media (max-width: 650px) {
+    text-align: left;
   }
+}
 
-  a {
-    margin-left: 30px;
-    font-size: 30px;
-    text-decoration: none;
-    color: #4A4A4A;
+a {
+  margin-left: 30px;
+  font-size: 30px;
+  text-decoration: none;
+  color: #4a4a4a;
 
-    @media (max-width: 650px) {
-      font-size: 15px;
-    }
+  @media (max-width: 650px) {
+    font-size: 15px;
   }
+}
 
-  .nuxt-link-exact-active {
-    border-bottom: 3px solid #4A4A4A;
-  }
+.donut {
+  margin: 0px;
+  position: absolute;
+  border: 6px solid #faae6e66;
+  border-radius: 100px;
+  left: 10px;
+  bottom: 20px;
+  width: 18px;
+  height: 18px;
+  transform: scale(1.4);
+  transform-origin: top left;
+}
+
+.nuxt-link-exact-active {
+  border-bottom: 3px solid #4a4a4a;
+}
 </style>
-
