a639e83ef2466182495982a0273debe40d16d2c8
Ben Winding <hello@benwinding.com>
2021-02-28 21:18:55 +1100

    More link

--- components/Header.vue
+++ components/Header.vue
@@ -1,10 +1,11 @@
 <template>
   <div>
-    <div class="text-right text-blue-800 text-xl md:text-2xl mt-4">
+    <div class="text-right text-blue-800 text md:text-2xl mt-4">
       <nuxt-link class="mr-3" to="/">Home</nuxt-link>
       <nuxt-link class="mr-3" to="/about">About</nuxt-link>
       <nuxt-link class="mr-3" to="/projects">Projects</nuxt-link>
       <nuxt-link class="mr-3" to="/contact">Contact</nuxt-link>
+      <span title="more of ben">[More]</span>
     </div>
     <a class="fixed border-8 border-red-400 opacity-50 bottom-0 left-0 w-6 h-6 m-2 rounded-full" href="/eat-the-donut.html"></a>
   </div>
