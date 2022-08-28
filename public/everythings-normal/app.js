7071424f4b932a1583854d14327bec03d4208ac9
Ben Winding <hello@benwinding.com>
2022-08-28 17:02:42 +1000

    Add to projects 👌

--- public/everythings-normal/app.js
+++ public/everythings-normal/app.js
@@ -1,48 +1,37 @@
 var scene = new THREE.Scene();
-var camera = new THREE.PerspectiveCamera( 75, window.innerWidth/window.innerHeight, 0.1, 1000 );
 var renderer = new THREE.WebGLRenderer({antialias:true});
 renderer.setClearColor(new THREE.Color('rgb(255, 255, 255)'));
 renderer.setSize( window.innerWidth, window.innerHeight );
 document.body.appendChild( renderer.domElement );
 
-/**
-
-  X: -1 to +1 :  Red:     0 to 255
-  Y: -1 to +1 :  Green:   0 to 255
-  Z:  0 to -1 :  Blue:  128 to 255
-
-  (0,0,-1) is mapped to (128,128,255)
-  (1,1,0) is mapped to (255,255,128)
-  (1,0,0) is mapped to (255,128,128)
-  (0,1,0) is mapped to (128,255,128)
-  (-1,0,0) is mapped to (0,128,128)
-  (0,-1,0) is mapped to (128,0,128)
-  (-1,-1,0) is mapped to (0,0,128)
-
-*/
 const lights = new THREE.Group();
 scene.add(lights);
-function addLight(directionXYZ, colorStr) {
-  const color = new THREE.Color(colorStr)
-  const lightTop = new THREE.DirectionalLight(color, 1);
-  lightTop.position.set( ...directionXYZ );
-  lights.add(lightTop);
-}
+function AddLights() {
+  /**
 
-addLight([0,0,-1], 'rgb(128,128,255)');
-addLight([1,1,0], 'rgb(255,255,128)');
-addLight([1,0,0], 'rgb(255,128,128)');
-addLight([0,1,0], 'rgb(128,255,128)');
-addLight([-1,0,0], 'rgb(0,128,128)');
-addLight([0,-1,0], 'rgb(128,0,128)');
-addLight([-1,-1,0], 'rgb(0,0,128)');
+    (0,0,-1) is mapped to (128,128,255)
+    (1,1,0) is mapped to (255,255,128)
+    (1,0,0) is mapped to (255,128,128)
+    (0,1,0) is mapped to (128,255,128)
+    (-1,0,0) is mapped to (0,128,128)
+    (0,-1,0) is mapped to (128,0,128)
+    (-1,-1,0) is mapped to (0,0,128)
 
-var cube;
-function addGeometry(geometry, shiftX) {
-  var material = new THREE.MeshPhongMaterial( { color: '#ddd' } );
-  cube = new THREE.Mesh( geometry, material );
-  scene.add( cube );
-  return cube;
+  */
+  function addLight(directionXYZ, colorStr) {
+    const color = new THREE.Color(colorStr)
+    const lightTop = new THREE.DirectionalLight(color, 1);
+    lightTop.position.set( ...directionXYZ );
+    lights.add(lightTop);
+  }
+
+  addLight([0,0,-1], 'rgb(128,128,255)');
+  addLight([1,1,0], 'rgb(255,255,128)');
+  addLight([1,0,0], 'rgb(255,128,128)');
+  addLight([0,1,0], 'rgb(128,255,128)');
+  addLight([-1,0,0], 'rgb(0,128,128)');
+  addLight([0,-1,0], 'rgb(128,0,128)');
+  addLight([-1,-1,0], 'rgb(0,0,128)');
 }
 
 const loader = new THREE.FontLoader();
@@ -61,22 +50,45 @@ function addText(text, opts, positionXYZ) {
       bevelSegments: 10,
       ...opts
     });
-    const mesh = addGeometry(geometry);
+    const material = new THREE.MeshPhongMaterial( { color: '#ddd' } );
+    const mesh = new THREE.Mesh( geometry, material );
     mesh.position.set(...positionXYZ);
+    scene.add(mesh);
   });
 }
 
+function AddWall() {
+  const geometry = new THREE.BoxGeometry(15, 10, 0.1);
+  const textureLoader = new THREE.TextureLoader()
+  textureLoader.load('bricks-normal-map.jpg', (texture) => {
+    texture.repeat.set(6,6)
+    texture.wrapS = true;
+    texture.wrapT = true;
+    const material = new THREE.MeshPhongMaterial( { 
+      bumpMap: texture,
+    } );
+    const mesh = new THREE.Mesh( geometry, material );
+    mesh.position.set(0, 0 , 0);
+    scene.add(mesh);
+  });
+}
+
+// Add stuff
+AddLights();
 addText('EVERYTHING\'S', {size: 0.4}, [-1.95,1,0])
-addText('NORMAL', {size: 0.7}, [-2,0,0])
+addText('NORMAL', {size: 0.7}, [-2,0,0]);
+AddWall();
+
+// Camera
+const camera = new THREE.PerspectiveCamera( 75, window.innerWidth/window.innerHeight, 0.1, 1000 );
+camera.position.z = 4;
+camera.position.x = -2;
+camera.position.y = 1;
 
 // Controls
 const controls = new THREE.OrbitControls(camera, renderer.domElement);
 controls.target.set(0, 0, 0);
 controls.update();
-
-camera.position.z = 4;
-camera.position.x = -2;
-camera.position.y = 1;
 
 // Render Loop
 var deg = 0;
