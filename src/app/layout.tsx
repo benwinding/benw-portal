2dc0306fb17804d45ac54458424aa249a30b3aa5
Ben Winding <hello@benwinding.com>
2024-03-17 14:05:50 +1100

    Remove unecessary stuff from layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,7 @@
-"use client";
 import React from "react";
 import "./global.css";
 import "../../public/fonts/font.css";
 import { Header } from "components/Header";
-import { CSSTransition, TransitionGroup } from "react-transition-group";
 
 export default function MyApp(props: React.PropsWithChildren) {
   return (
@@ -29,14 +27,7 @@ function DefaultLayout(props: { children: React.ReactNode }) {
     <div className="h-screen w-screen" style={{ overflowY: "overlay" as any, paddingRight: "10px" }}>
       <section className="max-w-6xl mx-auto p-2">
         <Header />
-        <TransitionGroup>
-          <CSSTransition
-            timeout={300}
-            classNames="item"
-          >
-            <>{props.children}</>
-          </CSSTransition>
-        </TransitionGroup>
+        {props.children}
       </section>
     </div>
   );
