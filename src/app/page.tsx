42b1c42312a4b9d81f1d45d6bdad0384a381fdab
Ben Winding <hello@benwinding.com>
2024-08-04 18:11:16 +1000

    Better front page!

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,27 +1,59 @@
+"use client";
+import classNames from "classnames";
 import { RainbowText } from "components/RainbowText";
-import Head from "next/head";
+import { ResultsList } from "components/search/ResultsList";
+import { useSearch } from "components/search/useSearch";
+import React from "react";
 
 export default function Page() {
+  const [hasText, setHasText] = React.useState(false);
+  const { setSearchText, results, blogsLoaded } = useSearch();
+
+  const onSearchChange = React.useCallback((searchText: string) => {
+    setHasText(!!searchText.trim());
+    setSearchText(searchText);
+  }, []);
+
   return (
-    <>
-      <Head>
-        <title>Home</title>
-        <meta property="og:image" content="/imgur/orqq5jB.jpg"></meta>
-        <meta property="og:title" content="Home - Ben Winding"></meta>
-        <meta property="og:description" content="A web developer from Adelaide, South Australia."></meta>
-      </Head>
+    <div className="flex flex-col gap-3 pt-2">
+      <RainbowText text="BenWinding" />
+      <Search
+        placeholder="Search instantaneously..."
+        onSearchChange={onSearchChange}
+      />
       <div
-        className="absolute left-0 m-0 sm:ml-3"
-        style={{
-          top: "50%",
-          transform: "translateY(-50%)",
-        }}
+        className={classNames("transition-opacity", {
+          "opacity-0": !blogsLoaded,
+          "opacity-100": blogsLoaded,
+        })}
       >
-        <div className="relative">
-          <RainbowText text="Ben Winding" large></RainbowText>
-          <p className="ml-2">Complex solutions create complex bugs...</p>
-        </div>
+        <h2
+          className={classNames("transition-opacity italic text-gray-500", {
+            "opacity-0": hasText,
+            "opacity-100": !hasText,
+          })}
+        >
+          Some recent stuff
+        </h2>
+        <ResultsList results={results} />
       </div>
-    </>
+    </div>
+  );
+}
+
+function Search(props: {
+  placeholder: string;
+  onSearchChange: (searchText: string) => void;
+}) {
+  return (
+    <div className="block w-full">
+      <input
+        onChange={e => props.onSearchChange(e.target.value)}
+        type="text"
+        className="w-full p-2 border-2 rounded-lg border-gray-500"
+        placeholder={props.placeholder}
+      >
+      </input>
+    </div>
   );
 }
